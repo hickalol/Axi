78928dd9d013a0a56f042455f0930ae09ad6fce8
Celeste Bell <68662842+veveriqa@users.noreply.github.com>
2023-11-14 18:31:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, Collection, GatewayIntentBits } = require("discord.js")
 const Discord = require("discord.js")
+const { token } = require('./config.json');
 const client = new Client({
   intents: [
     Discord.GatewayIntentBits.Guilds,
@@ -10,11 +11,26 @@ const client = new Client({
 
 const config = require("./src/config.js");
 const { readdirSync } = require("fs")
-let token = config.token
 
 client.on("ready", async () => {
     console.log(`AXIOMA DISCORD logged in as ${client.user.tag}.`);
     client.user.setPresence({ activities: [{ name: 'Crawling' }], status: 'idle' });
+});
+
+client.on("messageCreate", function(message) {
+  if (message.author == client.user)
+  return;
+  if (message.author.bot)
+  return;
+  if (message.content === 'a') {
+    message.channel.send('b');
+  }
+  if (message.content === 'owo') {
+    message.channel.send("uwu");
+  }
+  if (message.content === "uwu") {
+    message.channel.send("owo");
+  }
 });
 
 client.login(token)
